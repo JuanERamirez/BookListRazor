976f67ec1e5e134ebce02f78ee17ae34eda23132
Juan E Ramirez <juanrmrz017@gmail.com>
2020-10-02 10:30:40 -0700

    Created database and get handle for Books

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -8,9 +8,12 @@ namespace BookListRazor.Model
 {
     public class Book
     {
+        // Key tag, unique, automatically add ID as identity column to not
+        // have to pass the value, creates ID value 
         [Key]
         public int Id { get; set; }
 
+        // Required tag does not allow Name and Author to be null
         [Required]
         public string Name { get; set; }
         public string Author { get; set; }
